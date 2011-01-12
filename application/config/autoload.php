4f009651a46c108eec24f62f911792d5a6744aab
Dale Harvey <dale@arandomurl.com>
2011-01-12 00:39:21 +0000

    serve different css and javascript to mobile browsers

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -39,7 +39,7 @@
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'session', 'sauth');
+$autoload['libraries'] = array('database', 'session', 'sauth', 'user_agent');
 
 
 /*
--- application/views/shared/header.php
+++ application/views/shared/header.php
@@ -1,5 +1,10 @@
 <?php
 
+// probably not the best place for this
+$this->load->library('user_agent');
+$css = $this->agent->is_mobile() ? "mobile.css" : "forum.css";
+$js  = $this->agent->is_mobile() ? "mobile.js" : "forum.js";
+
 $this->load->model('user_dal');
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
@@ -7,8 +12,9 @@ $this->load->model('user_dal');
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<title>New Forum</title>
-	<link rel="stylesheet" type="text/css" href="/css/forum.css" />
+	<link rel="stylesheet" type="text/css" href="/css/<?php echo $css; ?>" />
 	<script type="text/javascript" src="/js/jquery-1.4.4.min.js"></script>
+	<script type="text/javascript" src="/js/<?php echo $js; ?>"></script>
 	
 	<base href="<?php echo site_url(); ?>" />
 </head>
