db888efe9ab956a77af6fc07c5294dc573b95402
Dale Harvey <dale@arandomurl.com>
2011-01-12 12:33:17 +0000

    move jquery back to header

--- application/views/shared/footer.php
+++ application/views/shared/footer.php
@@ -15,7 +15,6 @@ $js  = $this->agent->is_mobile() ? "mobile.js" : "forum.js";
 		
 	</div>
 
-	<script type="text/javascript" src="/js/jquery-1.4.4.min.js"></script>
 	<script type="text/javascript" src="/js/<?php echo $js; ?>"></script>
 	
 </body>
