315d58df10be004c958919cb12f41846a61e2695
Dale Harvey <dale@arandomurl.com>
2011-01-12 20:24:23 +0800

    move scripts to load at the bottom of the page

--- application/views/shared/footer.php
+++ application/views/shared/footer.php
@@ -1,4 +1,6 @@
-
+<?php
+$js  = $this->agent->is_mobile() ? "mobile.js" : "forum.js";
+?>
 			</div>
 			
 		</div>
@@ -12,6 +14,9 @@
 		stuff at the bottom
 		
 	</div>
+
+	<script type="text/javascript" src="/js/jquery-1.4.4.min.js"></script>
+	<script type="text/javascript" src="/js/<?php echo $js; ?>"></script>
 	
 </body>
 </html>
--- application/views/shared/header.php
+++ application/views/shared/header.php
@@ -2,20 +2,16 @@
 
 // probably not the best place for this
 $this->load->library('user_agent');
-$css = $this->agent->is_mobile() ? "mobile.css" : "forum.css";
-$js  = $this->agent->is_mobile() ? "mobile.js" : "forum.js";
-
 $this->load->model('user_dal');
+
+$css = $this->agent->is_mobile() ? "mobile.css" : "forum.css";
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<title>New Forum</title>
-	<link rel="stylesheet" type="text/css" href="/css/<?php echo $css; ?>" />
-	<script type="text/javascript" src="/js/jquery-1.4.4.min.js"></script>
-	<script type="text/javascript" src="/js/<?php echo $js; ?>"></script>
-	
+	<link rel="stylesheet" type="text/css" href="/css/<?php echo $css; ?>" />	
 	<base href="<?php echo site_url(); ?>" />
 </head>
 <body>
