00962e8c10950a2afac0bbcd29d1862906b89ed3
Dale Harvey <dale@arandomurl.com>
2011-01-17 02:27:46 +0000

    add client side code for title update

--- public/js/forum.js
+++ public/js/forum.js
@@ -7,6 +7,41 @@ var YAY = (function() {
             html = "<quote name=\"" + user + "\">" + post + "</quote>";
         $("#thread-content-input").val($("#thread-content-input").val() + html);
     });
+
+    (function () {
+        
+        var title, tpl = $("#title-input").html();
+    
+        $("#main-title").bind("click", function () {
+            if ($(this).is(":not(.editing)")) {
+                title = $(this).text();
+                $(this).addClass("editing");
+                $(this).empty().append(tpl);
+                var input = $(this).find("#title-input");
+                input.val(title);
+                input[0].focus();
+                input[0].select();
+            }
+        });
+        
+        $("#cancel-title").live("click", function () {
+            $("#main-title")
+                .empty()
+                .html("<h3>" + title + "</h3>")
+                .removeClass("editing");            
+        });
+
+        $("#save-title").live("click", function () {
+            alert("TODO!");
+            var newTitle = $("#title-input").val();
+            $("#main-title")
+                .empty()
+                .html("<h3>" + newTitle + "</h3>")
+                .removeClass("editing");            
+            // TODO: save the title
+        });
+
+    })();
     
 })();
 
