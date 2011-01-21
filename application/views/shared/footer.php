32fc3920f0fc524c0c0930ca52a5bccb3e77e4f4
Dale Harvey <dale@arandomurl.com>
2011-01-21 01:38:29 +0000

    fixed title bar changes

--- application/views/shared/footer.php
+++ application/views/shared/footer.php
@@ -9,7 +9,7 @@
 	<div id="bottom">
 	</div>
 
-	<script type="text/javascript" src="/js/<?php echo $js; ?>"></script>
+	<script type="text/javascript" src="/js/global.js"></script>
 <?php
   if ($this->agent->is_mobile()) { 
     echo 	'<script type="text/javascript" src="/js/mobile.js"></script>';
