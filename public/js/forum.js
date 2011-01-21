ea2742cc9c306f6822196f9aad90585f28122273
Dale Harvey <dale@arandomurl.com>
2011-01-21 01:33:08 +0000

    fixed quoting

--- public/js/forum.js
+++ public/js/forum.js
@@ -1,12 +1,4 @@
-var YAY = (function() {
-    
-    $(".quote").bind("mousedown", function() {
-        var cmnt = $(this).parents(".comment"),
-            user = cmnt.find(".username").text(),
-            post = cmnt.find(".content").html(),
-            html = "<quote name=\"" + user + "\">" + post + "</quote>";
-        $("#thread-content-input").val($("#thread-content-input").val() + html);
-    });
+var YAY = (function() {    
 
     (function () {
         
--- public/js/thread.js
+++ public/js/thread.js
@@ -156,3 +156,12 @@ function insertAtCaret(areaId,text) {
 	}
 	txtarea.scrollTop = scrollPos;
 }
+
+
+$(".quote").bind("mousedown", function() {
+    var cmnt = $(this).parents(".comment"),
+        user = cmnt.find(".username").text(),
+        post = cmnt.find(".content").html(),
+        html = "<quote name=\"" + user + "\">" + post + "</quote>";
+    $("#thread-content-input").val($("#thread-content-input").val() + html);
+});
