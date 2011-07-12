ef106bbe6cf4909860754099528b09c9a5bc934a
castis <0ranges!>
2011-07-12 17:24:12 -0400

    for culthero

--- application/views/thread.php
+++ application/views/thread.php
@@ -155,7 +155,7 @@ else
 
 // and now the reply form
 $content = array(
-	'name'	=> 'thread-content-input',
+	'name'	=> 'content',
 	'id'	=> 'thread-content-input',
 	'value' => set_value('content')
 );
