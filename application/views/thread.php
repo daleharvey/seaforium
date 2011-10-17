69e7d890acec2b092cf93b7950fd187cd1908927
Dale Harvey <dale@arandomurl.com>
2011-10-18 00:52:59 +0100

    link to usernames correctly

--- application/views/thread.php
+++ application/views/thread.php
@@ -1,29 +1,29 @@
-<?php 
+<?php
 
-$favorite = in_array($thread_id, $favorites) ? ' added' : ''; 
+$favorite = in_array($thread_id, $favorites) ? ' added' : '';
 
 $session_id = $this->session->userdata('session_id');
 
 $logged_in = $this->sauth->is_logged_in();
 $use_notifier = $logged_in && (int)$this->session->userdata('new_post_notification') === 1
 
-?> 
+?>
 				<div id="thread">
 					<div id="main-title">
 						<h3><?php echo $info['title'] ?></h3>
 						<?php if ($logged_in) { ?><a class="favourite<?php echo $favorite; ?>" rel="<?php echo $thread_id; ?>"></a><?php } ?>
 					</div>
-					
+
 					<div class="pagination top">
 						<?php echo $pagination; ?>
 					</div>
-				
+
 <?php
 
 $i = 0;
 
 // loop through and print out all the comments
-foreach($comment_result->result() as $row) { 
+foreach($comment_result->result() as $row) {
 
 	// if the comment has been deleted,
 	// go ahead and just put the deleted thing and start the loop over
@@ -37,16 +37,16 @@ foreach($comment_result->result() as $row) {
 <?php
 		continue;
 	}
-	
+
 	// alternating comment colors!!!
 	$alt = (!isset($alt) || $alt === false ? true : false);
-	
+
 	$my_thread = $row->user_id == $this->session->userdata('user_id');
-	
+
 	$edit_source = ($my_thread && strtotime($row->created) > time() - 3600) ? 'edit' : 'View Source';
-	
-	$url_safe_username = url_title($row->username, 'dash', TRUE);
-	
+
+	$url_safe_username = url_title($row->username, 'dash');
+
 	switch($row->acq_type)
 	{
 		case 1:
@@ -58,46 +58,46 @@ foreach($comment_result->result() as $row) {
 		default:
 			$acq = null;
 	}
-	
+
 	if ($row->acq_type == 2)
 	{
-?> 
+?>
 
-					<div id="ignore-for-<?php echo $row->comment_id; ?>" class="ignore-container" onclick="$('#comment-container-<?php echo $row->comment_id; ?>').toggle();"></div>
+                                        <div id="ignore-for-<?php echo $row->comment_id; ?>" class="ignore-container" onclick="$('#comment-container-<?php echo $row->comment_id; ?>').toggle();"></div>
 
-<?php } ?> 
+<?php } ?>
 					<div id="comment-<?php echo $row->comment_id; ?>" class="comment<?php echo $alt === false ? '' : ' alt'; echo $acq; ?>">
 						<div id="comment-container-<?php echo $row->comment_id; ?>" class="comment-container">
 							<div class="cmd-bar">
 								<span><a class="view-source" onclick="thread.view_source(<?php echo $row->comment_id; ?>); return false;"><?php echo $edit_source; ?></a></span>
-								<?php if ($logged_in) { ?><a class="quote" onclick="thread.quote(<?php echo $row->comment_id; ?>);">Quote</a><?php } ?> 
+								<?php if ($logged_in) { ?><a class="quote" onclick="thread.quote(<?php echo $row->comment_id; ?>);">Quote</a><?php } ?>
 							</div>
 							<div class="user-block">
-								<div class="username<?php echo $acq; ?>"><?php echo anchor('/user/'. $url_safe_username, $row->username); ?></div>
+								<div class="username<?php echo $acq; ?>"><a href="/user/<?php echo $url_safe_username; ?>"><?php echo $url_safe_username;?></a></div>
 								<div class="time"><?php echo timespan(strtotime($row->created), time()) ?></div>
-								
+
 								<div class="user-information" style="background: url(/img/emoticons/<?php echo (int)$row->emoticon === 1 ? $row->id : '0'; ?>.gif);">
 									<ul>
-									<?php if ($logged_in) { ?> 
+									<?php if ($logged_in) { ?>
 										<li><a href="/buddies/<?php echo $url_safe_username; ?>"><?php echo ($acq)? "Your $acq!" : 'BUDDY? ENEMY?'; ?></a></li>
 										<li><a href="/messages/send/<?php echo $url_safe_username; ?>">SEND A MESSAGE</a></li>
-									<?php } else { ?> 
+									<?php } else { ?>
 										<li>&nbsp;</li>
 										<li>&nbsp;</li>
-									<?php } ?> 
+									<?php } ?>
 									</ul>
 								</div>
-								
-								<?php if ($my_thread && $i === 0 && $starting === 0) { 
-								
+
+								<?php if ($my_thread && $i === 0 && $starting === 0) {
+
 									$session_id = $this->session->userdata('session_id');
-									
+
 									$set_nsfw_status = $info['nsfw'] === '1' ? 0 : 1;
 									$set_closed_status = $info['closed'] === '1' ? 0 : 1;
-									
+
 									$nsfw_text = $info['nsfw'] === '1' ? 'Unmark Naughty' : 'Mark Naughty';
 									$closed_text = $info['closed'] === '1' ? 'Open Thread' : 'Close Thread';
-								
+
 								?>
 								<div id="thread-control">
 									<p>THREAD ADMIN</p>
@@ -106,7 +106,7 @@ foreach($comment_result->result() as $row) {
 										<li id="control-closed">&middot; <span><?php echo $closed_text; ?></span></li>
 									</ul>
 								</div>
-								
+
 								<script type="text/javascript">
 									$('#control-nsfw span').bind('click', function(){
 										thread.set_status(<?php echo $row->thread_id; ?>, 'nsfw', <?php echo $set_nsfw_status; ?>, '<?php echo $session_id; ?>')
@@ -116,13 +116,13 @@ foreach($comment_result->result() as $row) {
 									});
 								</script>
 								<?php }	?>
-								
+
 							</div>
 							<div class="content-block">
 								<div class="content">
-<?php 
-echo $this->session->userdata('view_html') === '1' || $this->session->userdata('view_html') === false 
-	? _ready_for_display($row->content, array('username'=>$row->username, "url_safe_username"=>$url_safe_username)) 
+<?php
+echo $this->session->userdata('view_html') === '1' || $this->session->userdata('view_html') === false
+	? _ready_for_display($row->content, array('username'=>$row->username, "url_safe_username"=>$url_safe_username))
 	: nl2br(htmlentities($row->content));
 ?>
 								</div>
@@ -137,27 +137,27 @@ echo $this->session->userdata('view_html') === '1' || $this->session->userdata('
 					</div>
 
 					<div class="dotted-bar replypad"></div>
-<?php if (!$logged_in || $info['closed'] === '1' || $info['acq_type'] === 2) { 
-	
+<?php if (!$logged_in || $info['closed'] === '1' || $info['acq_type'] === 2) {
+
 	if ($info['closed'] === '1')
 	{
 	?>
-	
+
 					<h3>This thread is closed</h3>
-	
+
 	<?php
 	}
 	elseif ($info['acq_type'] === 2)
 	{
 	?>
-	
+
 					<h3>You cannot post in this thread</h3>
-	
+
 	<?php
 	}
-	
-	
-	
+
+
+
 }
 else
 {
@@ -170,13 +170,13 @@ $content = array(
 );
 
 ?>
-					
+
 					<div id="comment-preview" class="test-comment" style="display: none;">
 						<div class="comment-container">
 							<div class="user-block">
 								<div class="username">You!</div>
 								<div class="time">Seconds from now</div>
-								
+
 								<div class="user-information" style="background: url(/img/noavatar.gif);">
 									<ul>
 										<li>&nbsp;</li>
@@ -186,18 +186,18 @@ $content = array(
 							</div>
 							<div class="content-block">
 								<div class="content">
-									
+
 								</div>
 							</div>
 							<div style="clear: both;"></div>
 						</div>
 
 					</div>
-					
+
 					<div id="reply-lc">
-						
+
 						<h4>Post A Reply</h4>
-						
+
 						<div id="post-shortcuts">
 							<p>SHORTCUTS!</p>
 							<ul>
@@ -206,11 +206,11 @@ $content = array(
 								<li>&middot; <a href="#">Spoiler</a></li>
 							</ul>
 						</div>
-						
+
 					</div>
-					
+
 					<div id="reply-rc">
-						
+
 						<div id="pinkies">
 							<a href="javascript:insertAtCaret('thread-content-input', '[:)]');"><img src="/img/pinkies/11.gif" /></a>
 							<a href="javascript:insertAtCaret('thread-content-input', '[:(]');"><img src="/img/pinkies/01.gif" /></a>
@@ -234,28 +234,28 @@ $content = array(
 							<a href="javascript:insertAtCaret('thread-content-input', '[NH]');"><img src="/img/pinkies/26.gif" /></a>
 							<a href="javascript:insertAtCaret('thread-content-input', '[fbm]');"><img src="/img/pinkies/21.gif" /></a>
 						</div>
-						
+
 						<form method="post" action="<?php echo uri_string(); ?>" id="comment-form">
-							
+
 							<div class="input textarea">
-								<?php echo form_textarea($content); ?> 
+								<?php echo form_textarea($content); ?>
 							</div>
-							
+
 							<p>I, <?php echo $this->session->userdata('username'); ?>, do solemnly swear that in posting this comment I promise to be nice.</p>
-							
+
 							<button type="submit" id="submit-button">Agree &amp; Post</button> <button type="button" id="preview-button">Preview</button>
 						</form>
-						
+
 					</div>
 <?php if ($use_notifier) { ?>
 					<div id="notifications">
 						<a id="closenotify"></a>
 					</div>
-					
+
 					<script type="text/javascript">
 						var originalTitle = document.title,
 							currentNotification;
-						
+
 						function thread_notifier()
 						{
 							$.ajax({
@@ -263,12 +263,12 @@ $content = array(
 								success: function(data) {
 									if (data) {
 										var text = $(data).text();
-										
+
 										document.title = text.replace(" added", "") + " | " + originalTitle;
-										
+
 										if (text !== currentNotification) {
 											$("#notifier").remove();
-											currentNotification = text;                   
+											currentNotification = text;
 												$('#notifications').append(data).show();
 										}
 									}
@@ -286,11 +286,11 @@ $content = array(
 					</script>
 <?php } ?>
 
-<?php } ?> 
+<?php } ?>
 
 					<script type="text/javascript" src="/js/thread.js"></script>
 
-<?php if ($logged_in) { ?>					
+<?php if ($logged_in) { ?>
 					<script type="text/javascript">
 						$('.favourite').bind('click', function(){
 							button = $(this);
@@ -317,5 +317,5 @@ $content = array(
 							return;
 						});
 					</script>
-<?php } ?> 
+<?php } ?>
 				</div>
