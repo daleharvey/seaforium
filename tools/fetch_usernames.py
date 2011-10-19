4d842b029693a43c1f62de0a892e65d3ce869841
Dale Harvey <dale@arandomurl.com>
2011-10-19 02:58:24 +0100

    Add tool to fetch all usernames from yayhooray.com

---
+++ tools/fetch_usernames.py
@@ -0,0 +1,11 @@
+import requests
+import lxml
+from lxml import html
+
+for i in range(1, 457):
+    r = requests.get('http://www.yayhooray.com/users?page=%s' % i)
+    tree = lxml.html.fromstring(r.content)
+    elements = tree.find_class('user_username')
+    for el in elements:
+        print el.text_content()
+
