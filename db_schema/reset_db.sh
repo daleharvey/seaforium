b921cdb002f2e8d545b507488dfc841982c93ef0
Dale Harvey <dale@arandomurl.com>
2011-10-21 05:29:45 +0100

    only wipe the database, dont delete schema

--- db_schema/reset_db.sh
+++ db_schema/reset_db.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 #
-# Wipe the database and initialise the schema again
+# Wipe the database
 # $ ./reset_db username password
 
 cd `dirname $0`
 
-mysql -u$1 -p$2 <<< "DROP DATABASE new_forum; CREATE DATABASE new_forum;"
+mysql -u$1 -p$2 new_forum <<< "TRUNCATE acquaintances;"
+mysql -u$1 -p$2 new_forum <<< "TRUNCATE categories;"
+mysql -u$1 -p$2 new_forum <<< "TRUNCATE comments;"
+mysql -u$1 -p$2 new_forum <<< "TRUNCATE favorites;"
+mysql -u$1 -p$2 new_forum <<< "TRUNCATE login_attempts;"
+mysql -u$1 -p$2 new_forum <<< "TRUNCATE pm_content;"
+mysql -u$1 -p$2 new_forum <<< "TRUNCATE pm_inbox;"
+mysql -u$1 -p$2 new_forum <<< "TRUNCATE pm_outbox;"
+mysql -u$1 -p$2 new_forum <<< "TRUNCATE sessions;"
+mysql -u$1 -p$2 new_forum <<< "TRUNCATE threads;"
+mysql -u$1 -p$2 new_forum <<< "TRUNCATE titles;"
+mysql -u$1 -p$2 new_forum <<< "TRUNCATE users;"
+mysql -u$1 -p$2 new_forum <<< "TRUNCATE user_autologin;"
+mysql -u$1 -p$2 new_forum <<< "TRUNCATE user_profiles;"
+mysql -u$1 -p$2 new_forum <<< "TRUNCATE yh_invites;"
+mysql -u$1 -p$2 new_forum <<< "TRUNCATE yh_whitelist;"
 
-for file in *.sql; do
-    mysql -u$1 -p$2 new_forum < $file
-done
+echo "Completed reset!"
