f63c34793fd7b258c64f51f9c2e354c0bbd505ed
unknown <Matt@.(none)>
2011-10-24 21:43:32 -0400

    Smarter shortcuts

--- public/js/thread.js
+++ public/js/thread.js
@@ -186,21 +186,40 @@ function insertAtCaret(areaId,text) {
   var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ?
 	    "ff" : (document.selection ? "ie" : false ) );
 
-  var quote = text.indexOf('"');
-  quote = quote == -1 ? 0 : text.length - quote - 1;
-		
   if (br == "ie") {
     txtarea.focus();
     var range = document.selection.createRange();
+	var selection = range.text;
     range.moveStart ('character', -txtarea.value.length);
     strPos = range.text.length;
+	
   }
-  else if (br == "ff") strPos = txtarea.selectionStart;
+  else if (br == "ff") {
+	strPos = txtarea.selectionStart;
+	var selection = (txtarea.value).substring(strPos, txtarea.selectionEnd);
+  }
+  
+  var cursorOffset = text.indexOf('"');
+  if(cursorOffset == -1 && !selection) {
+	cursorOffset = text.indexOf('<', 1) - 1;
+  }
+  cursorOffset = cursorOffset == -1 ? 0 : text.length - cursorOffset - 1;
+  
+  var closeTag = text.indexOf("<", 1);
+  if(closeTag == -1) {
+	var removeOffset = 0;
+  } else {
+	text = text.substring(0, closeTag) + selection + text.substring(closeTag);
+	if(cursorOffset) {
+		cursorOffset += selection.length;
+	}
+	var removeOffset = selection.length;
+  }
 
   var front = (txtarea.value).substring(0,strPos);
-  var back = (txtarea.value).substring(strPos,txtarea.value.length);
+  var back = (txtarea.value).substring(strPos + removeOffset);
   txtarea.value=front+text+back;
-  strPos = strPos + text.length - quote;
+  strPos = strPos + text.length - cursorOffset;
   
   if (br == "ie") {
     txtarea.focus();
