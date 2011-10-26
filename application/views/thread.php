d247440ee1485e760f933b257a1935e489ec9c1a
Dale Harvey <dale@arandomurl.com>
2011-10-26 01:11:08 +0100

    no short tags

--- application/views/thread.php
+++ application/views/thread.php
@@ -10,7 +10,7 @@ $owner = $logged_in && $this->session->userdata('user_id') == $info['user_id'];
 ?>
 
 <div id="thread">
-  <div id="main-title"<?=$owner ? ' class="changeling"':''?>><h3><?php echo $info['title'] ?></h3>
+  <div id="main-title"<?php echo $owner ? ' class="changeling"':''?>><h3><?php echo $info['title'] ?></h3>
     <?php if ($logged_in) { ?>
       <a class="favourite<?php echo $favorite; ?>"
          rel="<?php echo $thread_id; ?>"></a>
@@ -24,7 +24,7 @@ $owner = $logged_in && $this->session->userdata('user_id') == $info['user_id'];
 			<input type="button" value="Cancel" id="cancel-title" />
 		</script>
   <?php }?>
-	
+
   <div class="pagination top">
     <?php echo $pagination; ?>
   </div>
