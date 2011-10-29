d2448099ed37c443b4e7fdd3c1f6202595adf7d4
Dale Harvey <dale@arandomurl.com>
2011-10-30 00:11:34 +0100

    removed duplicate statement

--- db_schema/21_add_foreign_keys_and_drop_myISAM_for_performance.sql
+++ db_schema/21_add_foreign_keys_and_drop_myISAM_for_performance.sql
@@ -1,6 +1,5 @@
 ALTER TABLE `threads`  ENGINE=InnoDB,  DROP INDEX `subject`;
 ALTER TABLE `threads`  ADD CONSTRAINT `FK_threads_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION;
-ALTER TABLE `threads`  ADD CONSTRAINT `FK_threads_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION;
 ALTER TABLE `acquaintances`  ADD CONSTRAINT `FK_acquaintances_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION;
 ALTER TABLE `comments`  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION;
 ALTER TABLE `comments`  ADD CONSTRAINT `FK_comments_threads` FOREIGN KEY (`thread_id`) REFERENCES `threads` (`thread_id`) ON UPDATE NO ACTION ON DELETE NO ACTION;
