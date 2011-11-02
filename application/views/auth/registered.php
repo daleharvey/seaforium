c29e17aa0a9bd5e1906c5f80aa517df4a152b773
castis <castis@gmail.com>
2011-11-02 09:55:13 -0400

    registration page redone. captcha added.

---
+++ application/views/auth/registered.php
@@ -0,0 +1,3 @@
+<div id="main-title"><h3>OMG, you can Register?</h3></div>
+
+<div id="register-notice">SUCCESS! You need to activate your account, an activation link has been sent to you as a pm on <a href="http://yayhooray.com">yayhooray.com</a></div>
