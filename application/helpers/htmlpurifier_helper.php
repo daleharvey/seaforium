9852059299fc16d80df57843a86728f726b79fed
Dale Harvey <dale@arandomurl.com>
2011-11-04 16:44:46 +0100

    Fix new lines in posts

--- application/helpers/htmlpurifier_helper.php
+++ application/helpers/htmlpurifier_helper.php
@@ -56,7 +56,7 @@ function purify($dirty_html)
   $config = HTMLPurifier_Config::createDefault();
   $config->set('HTML.Doctype', 'XHTML 1.0 Strict');
   $config->set('AutoFormat.Linkify', true);
-  $config->set('AutoFormat.AutoParagraph', true);
+  $config->set('Output.Newline', '<br />');
   $config->set('Filter.YouTube', true);
   $def = $config->getHTMLDefinition(true);
   $def->addElement(
