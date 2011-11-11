1a8e26369e4d9e020c2ebcaabea2cf613f4a390d
Dale Harvey <dale@arandomurl.com>
2011-11-11 10:57:27 +0000

    remove some unuused code

--- application/helpers/htmlpurifier_helper.php
+++ application/helpers/htmlpurifier_helper.php
@@ -22,25 +22,8 @@ class HTMLPurifier_Filter_YouTube extends HTMLPurifier_Filter
                         preg_replace($pre_regex, $pre_replace, $html));
   }
 
-  /* public function postFilter($html, $config, $context) { */
-  /*   $post_regex = '#<span class="youtube-embed">((?:v|cp)/[A-Za-z0-9\-_=]+)</span>#'; */
-  /*   return preg_replace_callback($post_regex, array($this, 'postFilterCallback'), */
-  /*                                $html); */
-  /* } */
   protected function armorUrl($url) {
     return str_replace('--', '-&#45;', $url);
-  }
-  protected function postFilterCallback($matches) {
-    $url = $this->armorUrl($matches[1]);
-    return '<object width="425" height="350" type="application/x-shockwave-flash" '.
-      'data="http://www.youtube.com/'.$url.'">'.
-      '<param name="movie" value="http://www.youtube.com/'.$url.'"></param>'.
-      '<!--[if IE]>'.
-      '<embed src="http://www.youtube.com/'.$url.'"'.
-      'type="application/x-shockwave-flash"'.
-      'wmode="transparent" width="425" height="350" />'.
-      '<![endif]-->'.
-      '</object>';
   }
 }
 
