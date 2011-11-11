5f934ac310f3935f30dbe93b55becc925509be49
Dale Harvey <dale@arandomurl.com>
2011-11-11 03:25:28 +0000

    fixed sizing for <object> embeds

--- application/helpers/htmlpurifier_helper.php
+++ application/helpers/htmlpurifier_helper.php
@@ -9,10 +9,17 @@ class HTMLPurifier_Filter_YouTube extends HTMLPurifier_Filter
   public $name = 'YouTube';
 
   public function preFilter($html, $config, $context) {
-    $pre_regex = '#<object[^>]+>.+?'.
+
+    $pre_regex = '#<object.*width="(\d+)".*height="(\d+)".*>.+?'.
       'http://www.youtube.com/((?:v|cp)/[A-Za-z0-9\-_=]+).+?</object>#s';
-    $pre_replace = 'http://youtube.com/watch?v=\1</span>';
-    return preg_replace($pre_regex, $pre_replace, $html);
+    $pre_replace = 'http://youtube.com/watch?v=\3&size=\1x\2</span>';
+
+    $pre_regex2 = '#<object[^>]+>.+?'.
+      'http://www.youtube.com/((?:v|cp)/[A-Za-z0-9\-_=]+).+?</object>#s';
+    $pre_replace2 = 'http://youtube.com/watch?v=\1</span>';
+
+    return preg_replace($pre_regex2, $pre_replace2,
+                        preg_replace($pre_regex, $pre_replace, $html));
   }
 
   /* public function postFilter($html, $config, $context) { */
