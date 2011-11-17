bda67354036cf712f00fde61696c1da802b2fb09
Dale Harvey <dale@arandomurl.com>
2011-11-17 02:59:43 +0000

    Add instuctions and make setup easier

--- tests/pytests/DbClient.py
+++ tests/pytests/DbClient.py
@@ -11,20 +11,27 @@ import os
 
 def reset_database(host, database, username, password):
 
-    con = mdb.connect(host, username, password, database);
-
+    con = mdb.connect(host, username, password);
     cur = con.cursor()
+
+    try:
+        cur.execute('CREATE DATABASE %s;' % database)
+    except:
+        pass
+
     cur.execute('DROP DATABASE %s' % database)
     cur.execute('CREATE DATABASE %s;' % database)
 
     schema_dir = os.environ['yay_root'] + "/db_schema"
-    cmd = "/usr/local/mysql/bin/mysql -u'%s' -p'%s' %s < %s/%s"
+    # OSX has problems finding mysql on PATH, might need
+    # to change to /usr/local/mysql/bin/mysql
+    cmd = "mysql -u'%s' -p'%s' %s < %s/%s"
 
     schemas = os.listdir(schema_dir)
-    for fname in schemas:
+    for fname in sorted(schemas):
         if fname.endswith('.sql') and not fname == "13_import_yayusers.sql":
             call(cmd % (username, password, database, schema_dir, fname), shell=True)
 
     print "Import Complete!"
-	
+
     con.close()
