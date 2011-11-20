1198eceab3dba2b8146db10a39ba7e9c8d6e93db
Dale Harvey <dale@arandomurl.com>
2011-11-20 12:42:04 +0000

    minor refactor

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -24,10 +24,11 @@ class Welcome extends Controller {
 
   function index($pagination = 0, $filter = '', $ordering = '', $dir = 'desc', $whostarted = '')
   {
-	// uncomment the following line you if broke something but you can't figure out what.
-	//$this->output->enable_profiler(TRUE);
-	if (strtolower($filter) == 'started' && $whostarted == '')
-		$whostarted = $this->meta['username'];
+    // uncomment the following line you if broke something but you can't figure out what.
+    //$this->output->enable_profiler(TRUE);
+    if (strtolower($filter) == 'started' && $whostarted == '') {
+      $whostarted = $this->meta['username'];
+    }
 
     $filtering = $this->_ready_filters($filter, $ordering, $dir, $whostarted);
 
@@ -53,10 +54,15 @@ class Welcome extends Controller {
     // end of threads
     $end = min(array($pagination + $display, $thread_count));
 
+    $thread_result = $this->thread_dal->get_threads($this->meta['user_id'], $pagination, $display,
+                                                    $filtering['filter'], $filtering['order']);
+    $pages = $this->pagination->create_links() . '<span class="paging-text">' .
+      ($pagination + 1) . ' - ' . $end . ' of ' . $thread_count . ' Threads</span>';
+
     $this->load->view('threads', array(
       'title' => $this->thread_dal->get_front_title(),
-      'thread_result' => $this->thread_dal->get_threads($this->meta['user_id'], $pagination, $display, $filtering['filter'], $filtering['order']),
-      'pagination' => $this->pagination->create_links() . '<span class="paging-text">' . ($pagination + 1) . ' - ' . $end . ' of ' . $thread_count . ' Threads</span>',
+      'thread_result' => $thread_restult,
+      'pagination' => $pages,
       'tab_links' => strlen($filter) > 0 ? '/f/'.$filter.'/' : '/o/',
       'tab_orders' => array(
         'started' => $ordering == 'started' && $dir == 'desc' ? 'asc' : 'desc',
@@ -92,42 +98,48 @@ class Welcome extends Controller {
         $sql = "WHERE threads.category != 4";
         break;
       case 'participated':
-        $sql = "WHERE threads.thread_id IN (". $this->thread_dal->get_participated_threads($this->meta['user_id']) .")";
+        $sql = "WHERE threads.thread_id IN (" .
+          $this->thread_dal->get_participated_threads($this->meta['user_id']) .")";
         break;
       case 'favorites':
-        $sql = "WHERE threads.thread_id IN (". $this->thread_dal->get_favorites($this->meta['user_id']) .")";
+        $sql = "WHERE threads.thread_id IN (" .
+          $this->thread_dal->get_favorites($this->meta['user_id']) .")";
         break;
       case 'hidden':
-        $sql = "WHERE threads.thread_id IN (". $this->thread_dal->get_hidden($this->meta['user_id']) .")";
+        $sql = "WHERE threads.thread_id IN (" .
+          $this->thread_dal->get_hidden($this->meta['user_id']) .")";
         break;
       case 'started':
-		if ($whostarted!='') {
-			$whostartedid = $this->user_dal->get_user_id_by_username($whostarted);
-			if ($whostartedid===FALSE) $whostartedid = $this->meta['user_id'];
-		}else{
-			$whostartedid = $this->meta['user_id'];
-		}
-        $sql = "WHERE threads.thread_id IN (". $this->thread_dal->get_started_threads($whostartedid) .")";
+        if ($whostarted!='') {
+          $whostartedid = $this->user_dal->get_user_id_by_username($whostarted);
+          if ($whostartedid===FALSE) $whostartedid = $this->meta['user_id'];
+        }else{
+          $whostartedid = $this->meta['user_id'];
+        }
+        $sql = "WHERE threads.thread_id IN (" .
+          $this->thread_dal->get_started_threads($whostartedid) .")";
         break;
       case 'all':
       default:
         $filter = $sql = '';
     }
 
-	$sql .= $sql ? ' AND' : 'WHERE';
+    $sql .= $sql ? ' AND' : 'WHERE';
 
-	if (strtolower($filter) != 'hidden')
-	{
-		$sql .= "  NOT EXISTS (SELECT hidden_threads.hidden_id FROM hidden_threads WHERE hidden_threads.user_id = ". $this->meta['user_id'] ." AND hidden_threads.thread_id = threads.thread_id) ";
-		$sql .= ' AND threads.deleted = 0';
-	} else {
-		$sql .= ' threads.deleted = 0';
-	}
+    if (strtolower($filter) != 'hidden') {
+      $sql .= "  NOT EXISTS (SELECT hidden_threads.hidden_id FROM hidden_threads WHERE " .
+        "hidden_threads.user_id = ". $this->meta['user_id'] ." AND hidden_threads.thread_id " .
+        "= threads.thread_id) ";
+      $sql .= ' AND threads.deleted = 0';
+    } else {
+      $sql .= ' threads.deleted = 0';
+    }
 
 
     // make sure the direction is one or the other
-    if (!in_array(strtolower($dir), array('desc', 'asc')))
+    if (!in_array(strtolower($dir), array('desc', 'asc'))) {
       $dir = 'desc';
+    }
 
     switch(strtolower($ordering))
       {
@@ -146,13 +158,13 @@ class Welcome extends Controller {
       }
 
     return array(
-                 'filter' => $sql,
-                 'order' => $sql_order,
-                 'url_suffix' => (strlen($filter) > 0 ? '/'.$filter : '')
-                 . (strlen($ordering) > 0 ? '/'.$ordering : '')
-                 . (strlen($dir) > 0 ? '/'.$dir : '')
-                 . (strlen($whostarted) > 0 ? '/'.$whostarted : '')
-                 );
+      'filter' => $sql,
+      'order' => $sql_order,
+      'url_suffix' => (strlen($filter) > 0 ? '/'.$filter : '')
+        . (strlen($ordering) > 0 ? '/'.$ordering : '')
+        . (strlen($dir) > 0 ? '/'.$dir : '')
+        . (strlen($whostarted) > 0 ? '/'.$whostarted : '')
+    );
   }
 }
 
