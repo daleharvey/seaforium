c561eea4a062580c22087ceb6378b404b4767685
Dale Harvey <dale@arandomurl.com>
2011-11-20 11:54:55 +0000

    mostly jslint changes, fixed quoted videos

--- public/js/thread.js
+++ public/js/thread.js
@@ -1,46 +1,47 @@
 function cloneObj(obj) {
   return jQuery.extend(true, {}, obj);
-};
+}
 
 jQuery.fn.reverse = function() {
-    return this.pushStack(this.get().reverse(), arguments);
+  return this.pushStack(this.get().reverse(), arguments);
 };
 
 $.fn.selectRange = function(start, end) {
-	if(!end) end = start;
+  if (!end) {
+    end = start;
+  }
 
-    return this.each(function() {
-        if (this.setSelectionRange) {
-            this.focus();
-            this.setSelectionRange(start, end);
-        } else if (this.createTextRange) {
-            var range = this.createTextRange();
-            range.collapse(true);
-            range.moveEnd('character', end);
-            range.moveStart('character', start);
-            range.select();
-        }
-    });
+  return this.each(function() {
+    if (this.setSelectionRange) {
+      this.focus();
+      this.setSelectionRange(start, end);
+    } else if (this.createTextRange) {
+      var range = this.createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', end);
+      range.moveStart('character', start);
+      range.select();
+    }
+  });
 };
 
 function format_special(element)
 {
-
   $('spoiler').each(function() {
     var html = '<div class="spoiler">' +
       '<div class="spoiler-disclaimer">Warning! May contain spoilers</div>' +
       '<div class="spoiler-content">' + $(this).html() + '</div>' +
-      '</div>'
+      '</div>';
     $(this).replaceWith(html);
   });
 
   var ytube = new RegExp('(?:")?http(?:s)?://(?:www.)?youtu(?:be)?.(?:[a-z]){2,3}' +
-                         '(?:[a-z/?=]+)([a-zA-Z0-9-_]{11})(?:[a-z0-9\?\&\-_=]+)?');
-  var vimeo = new RegExp('http(?:s)?://(?:www.)?vimeo.com/([0-9]+)(?:#[a-z0-9\?\&\-_=]*)?');
+                         '(?:[a-z/?=]+)([a-zA-Z0-9-_]{11})(?:[a-z0-9?&-_=]+)?');
+  var vimeo = new RegExp('http(?:s)?://(?:www.)?vimeo.com/([0-9]+)(?:#[a-z0-9?&-_=]*)?');
 
   $(element).each(function(){
 
-      var text = $(this).find("*").andSelf().contents().each(function () {
+    var text = $(this).find("*").andSelf().contents().each(function () {
 
       if (this.nodeType !== 3 || this.parentNode.nodeName === 'A') {
         return;
@@ -65,7 +66,7 @@ function format_special(element)
           }
 
           if (element[0] == 'size') {
-            var width_height_params = element[1].split("x")
+            var width_height_params = element[1].split("x");
             video_width = width_height_params[0];
             video_height = width_height_params[1];
           }
@@ -76,14 +77,14 @@ function format_special(element)
           video_url_append_string += video_url_append[m];
         }
 
+        var youtube_html;
         if (a.indexOf("\"") != -1) {
-          var youtube_html = a;
+          youtube_html = a;
         } else {
-          var youtube_html = "<div style='width:" + video_width + "px; height:" +
+          youtube_html = "<div style='width:" + video_width + "px; height:" +
             video_height + "px' id='" + b + "' class='youtube_wrapper' " +
             "data-extra='" + video_url_append_string + "'></div><br/>";
         }
-
         return youtube_html;
       });
 
@@ -100,6 +101,17 @@ function format_special(element)
 
     });
 
+    // Reverse so we handle nested quotes
+    $(this).find('blockquote').reverse().each(function(){
+      var user = $(this).attr('title') || 'Someone';
+      $(this).after(
+	$('<div>', {
+	  'class': 'tquote',
+	  'html': '<div class="tqname">' + user + ' said:</div>'+$(this).html()
+	})
+      ).remove();
+    });
+
     $(this).find('.youtube_wrapper').each(function() {
       var self = this;
       $.ajax({
@@ -111,17 +123,6 @@ function format_special(element)
                          data.data.title + '</h2><div class="youtube_playbutton"></div>');
         }
       });
-    });
-
-    // Reverse so we handle nested quotes
-    $(this).find('blockquote').reverse().each(function(){
-      var user = $(this).attr('title') || 'Someone';
-      $(this).after(
-	$('<div>', {
-	  'class': 'tquote',
-	  'html': '<div class="tqname">' + user + ' said:</div>'+$(this).html()
-	})
-      ).remove();
     });
   });
 }
@@ -140,10 +141,10 @@ $('#preview-button').live('click', function(e){
 });
 
 $("#comment-form").live("submit", function() {
-  if ($("#thread-content-input").val().length == 0)
+  if ($("#thread-content-input").val().length === 0) {
     return false;
-
-  $("#submit-button").attr('disabled', 'disabled')
+  }
+  $("#submit-button").attr('disabled', 'disabled');
   this.submit();
 });
 
@@ -153,43 +154,45 @@ selected = {
 };
 
 $('.content').click(function() {
-	selected.html = null;
-	selected.comment_id = null;
+  selected.html = null;
+  selected.comment_id = null;
 
-	if(window.getSelection) {
-		// not IE case
-		selObj = window.getSelection();
-		if(selObj.focusNode) {
-			selRange = selObj.getRangeAt(0);
+  if(window.getSelection) {
+    // not IE case
+    selObj = window.getSelection();
+    if(selObj.focusNode) {
+      selRange = selObj.getRangeAt(0);
 
-			p = selRange.commonAncestorContainer;
-			while(p.parentNode && !$(p).hasClass("comment-container")) {
-				p = p.parentNode;
-			}
+      p = selRange.commonAncestorContainer;
+      while(p.parentNode && !$(p).hasClass("comment-container")) {
+	p = p.parentNode;
+      }
 
-			if(p.id) {
-				dash = p.id.lastIndexOf('-');
+      if(p.id) {
+	dash = p.id.lastIndexOf('-');
 
-				if(dash != -1) {
-					selected.comment_id = p.id.substring(dash+1);
+	if(dash != -1) {
+	  selected.comment_id = p.id.substring(dash+1);
 
-					fragment = selRange.cloneContents();
-					e = document.createElement('b');
-					e.appendChild(fragment);
-					selected.html = e.innerHTML;
-				}
-			}
-
-			//selObj.removeAllRanges();
-		}
-	} else if (document.selection && document.selection.createRange && document.selection.type != "None") {
-		// IE case
-		selected.html = document.selection.createRange().htmlText;
+	  fragment = selRange.cloneContents();
+	  e = document.createElement('b');
+	  e.appendChild(fragment);
+	  selected.html = e.innerHTML;
 	}
+      }
+      //selObj.removeAllRanges();
+    }
+  } else if (document.selection &&
+             document.selection.createRange &&
+             document.selection.type != "None") {
+    // IE case
+    selected.html = document.selection.createRange().htmlText;
+  }
 });
 
 thread = {
-  status_text: {'nsfw': ['Unmark Naughty', 'Mark Naughty'], 'closed': ['Open Thread', 'Close Thread']},
+  status_text: {'nsfw': ['Unmark Naughty', 'Mark Naughty'],
+                'closed': ['Open Thread', 'Close Thread']},
   comments: [],
 
   get_comment_details: function(comment_id, callback)
@@ -197,8 +200,7 @@ thread = {
     $.ajax({
       url: '/ajax/view_source/'+comment_id,
       success: function(data) {
-	if (data)
-	{
+	if (data) {
 	  container = $('#comment-'+comment_id+' .content');
 
 	  // set the originals
@@ -217,30 +219,29 @@ thread = {
 
   quote: function(comment_id)
   {
-    if (thread.comments[comment_id] != undefined)
-    {
-		if(selected.comment_id && selected.comment_id != comment_id || !selected.html) {
-			content = thread.comments[comment_id].data.content;
-		} else {
-			content = selected.html;
-		}
+    if (thread.comments[comment_id] !== undefined) {
+      if(selected.comment_id && selected.comment_id != comment_id || !selected.html) {
+	content = thread.comments[comment_id].data.content;
+      } else {
+	content = selected.html;
+      }
 
-		selected.html = null;
-		selected.comment_id = null;
+      selected.html = null;
+      selected.comment_id = null;
 
-		html = "<blockquote title=\"" + $.trim(thread.comments[comment_id].author) + "\">\n" + content + "\n</blockquote>";
+      html = "<blockquote title=\"" + $.trim(thread.comments[comment_id].author) +
+        "\">\n" + content + "\n</blockquote>";
 
-		$("#thread-content-input").val($("#thread-content-input").val() + html);
+      $("#thread-content-input").val($("#thread-content-input").val() + html);
 
-		$(window).scrollTop($("#thread-content-input").offset().top);
-		$("#thread-content-input").focus();
-		$("#thread-content-input").scrollTop($("#thread-content-input")[0].scrollHeight - $("#thread-content-input").height());
-		$("#thread-content-input").selectRange($("#thread-content-input").val().length);
-    }
-    else
-    {
+      $(window).scrollTop($("#thread-content-input").offset().top);
+      $("#thread-content-input").focus();
+      $("#thread-content-input").scrollTop($("#thread-content-input")[0].scrollHeight -
+                                           $("#thread-content-input").height());
+      $("#thread-content-input").selectRange($("#thread-content-input").val().length);
+    } else {
       thread.get_comment_details(comment_id, function(){
-		thread.quote(comment_id);
+	thread.quote(comment_id);
       });
     }
   },
@@ -250,7 +251,7 @@ thread = {
     var post_data = {
       comment_id: comment_id,
       content: $('#comment-'+comment_id+' .content textarea').val()
-    }
+    };
 
     $.ajax({
       type: 'POST',
@@ -269,26 +270,25 @@ thread = {
     $.get(
       '/ajax/set_thread_status/'+ thread_id +'/'+ keyword +'/'+ status +'/'+ key,
       function(data) {
-		if (data == 1)
-		{
-			if(keyword == 'deleted') {
-				location = '/';
-			} else {
-				status = status == 1 ? 0 : 1;
+	if (data == 1) {
+	  if(keyword == 'deleted') {
+	    location = '/';
+	  } else {
+	    status = status == 1 ? 0 : 1;
 
-				$('#control-'+ keyword +' span').unbind('click').bind('click', function(){
-					thread.set_status(thread_id, keyword, status, key);
-					return false;
-				}).html(thread.status_text[keyword][status]);
-			}
-		}
+	    $('#control-'+ keyword +' span').unbind('click').bind('click', function(){
+	      thread.set_status(thread_id, keyword, status, key);
+	      return false;
+	    }).html(thread.status_text[keyword][status]);
+	  }
+	}
       }
     );
   },
 
   view_original: function(comment_id)
   {
-    if (thread.comments[comment_id] != undefined) {
+    if (thread.comments[comment_id] !== undefined) {
       $('#comment-' + comment_id + ' .content')
         .html(thread.comments[comment_id].rendered);
     }
@@ -296,7 +296,7 @@ thread = {
 
   view_source: function(comment_id)
   {
-    if (thread.comments[comment_id] != undefined) {
+    if (thread.comments[comment_id] !== undefined) {
 
       comment = thread.comments[comment_id];
 
@@ -330,27 +330,29 @@ thread = {
 
   id: function()
   {
-	return $('.favourite').attr('rel');
+    return $('.favourite').attr('rel');
   }
-}
+};
 
 function insertAtCaret(areaId,text) {
   var txtarea = document.getElementById(areaId);
   var scrollPos = txtarea.scrollTop;
   var strPos = 0;
+  var selection;
+  var removeOffset;
+  var range;
   var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ?
 	    "ff" : (document.selection ? "ie" : false ) );
 
   if (br == "ie") {
     txtarea.focus();
-    var range = document.selection.createRange();
-	var selection = range.text;
+    range = document.selection.createRange();
+    selection = range.text;
     range.moveStart ('character', -txtarea.value.length);
     strPos = range.text.length;
-  }
-  else if (br == "ff") {
-	strPos = txtarea.selectionStart;
-	var selection = (txtarea.value).substring(strPos, txtarea.selectionEnd);
+  } else if (br == "ff") {
+    strPos = txtarea.selectionStart;
+    selection = (txtarea.value).substring(strPos, txtarea.selectionEnd);
   }
 
   var cursorOffset = text.indexOf('"');
@@ -361,13 +363,13 @@ function insertAtCaret(areaId,text) {
 
   var closeTag = text.indexOf("<", 1);
   if(closeTag == -1) {
-	var removeOffset = 0;
+    removeOffset = 0;
   } else {
-	text = text.substring(0, closeTag) + selection + text.substring(closeTag);
-	if(cursorOffset) {
-		cursorOffset += selection.length;
-	}
-	var removeOffset = selection.length;
+    text = text.substring(0, closeTag) + selection + text.substring(closeTag);
+    if(cursorOffset) {
+      cursorOffset += selection.length;
+    }
+    removeOffset = selection.length;
   }
 
   var front = (txtarea.value).substring(0,strPos);
@@ -377,7 +379,7 @@ function insertAtCaret(areaId,text) {
 
   if (br == "ie") {
     txtarea.focus();
-    var range = document.selection.createRange();
+    range = document.selection.createRange();
     range.moveStart ('character', -txtarea.value.length);
     range.moveStart ('character', strPos);
     range.moveEnd ('character', 0);
