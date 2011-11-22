6371c2bb7571cafafb4b86ca1cd9198d02f20ad5
Dale Harvey <dale@arandomurl.com>
2011-11-22 04:53:13 +0000

    Fixed minor syntax error #29

--- db_schema/28_more_fks.sql
+++ db_schema/28_more_fks.sql
@@ -10,5 +10,5 @@ ALTER TABLE `pm_outbox`  ADD CONSTRAINT `FK_pm_outbox_to_id` FOREIGN KEY (`to_id
 ALTER TABLE `pm_outbox`  ADD CONSTRAINT `FK_pm_outbox_from_id` FOREIGN KEY (`from_id`) REFERENCES `users` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION;
 ALTER TABLE `pm_outbox`  ADD CONSTRAINT `FK_pm_outbox_message_id` FOREIGN KEY (`message_id`) REFERENCES `pm_content` (`message_id`) ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-ALTER TABLE  `threads` CHANGE  `category`  `category` INT( 2 ) NULL DEFAULT  '0'
+ALTER TABLE  `threads` CHANGE  `category`  `category` INT( 2 ) NULL DEFAULT  '0';
 ALTER TABLE `threads`  ADD CONSTRAINT `FK_threads_category` FOREIGN KEY (`category`) REFERENCES `categories` (`category_id`) ON UPDATE NO ACTION ON DELETE NO ACTION;
