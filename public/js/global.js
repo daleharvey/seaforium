9a745b3e83828e503c93a60cd14b4b8d7a31e043
Dale Harvey <dale@arandomurl.com>
2011-11-23 22:56:23 +0000

    Mostly jslint fixes

--- public/js/global.js
+++ public/js/global.js
@@ -1,18 +1,18 @@
 
 (function () {
-    var title, tpl = $("#title-input").html();
+  var title, tpl = $("#title-input").html();
 
-    $("#main-title.changeling").bind("click", function () {
-      if ($(this).is(":not(.editing)")) {
-        title = $('h3', this).text();
-        $(this).addClass("editing");
-        $('h3', this).empty().append(tpl);
-        var input = $(this).find("#title-input");
-        input.val(title);
-        input[0].focus();
-        input[0].select();
-      }
-    });
+  $("#main-title.changeling").bind("click", function () {
+    if ($(this).is(":not(.editing)")) {
+      title = $('h3', this).text();
+      $(this).addClass("editing");
+      $('h3', this).empty().append(tpl);
+      var input = $(this).find("#title-input");
+      input.val(title);
+      input[0].focus();
+      input[0].select();
+    }
+  });
 
   $('.youtube_wrapper').live("click", function() {
     var obj = $(this);
@@ -23,83 +23,67 @@
                    obj.attr('id') + '?autoplay=1' + obj.data('extra') +
                    '" frameborder="0" allowfullscreen></iframe><br />');
     }
-  })
+  });
 
-    $("#cancel-title").live("click", function () {
-      $('h3', "#main-title")
-        .empty()
-        .text(title);
-	  $("#main-title")
-        .removeClass("editing");
+  $("#cancel-title").live("click", function () {
+    $('h3', "#main-title").empty().text(title);
+    $("#main-title").removeClass("editing");
+  });
+
+  $("#save-title").live("click", function () {
+    var newTitle = $("#title-input").val();
+    var data = "title=" + newTitle;
+    data += isThread() ? "&thread_id=" + thread.id() : '';
+
+    $.ajax({
+      type: "POST",
+      url: "/title/edit",
+      data: data,
+      success: function(msg){
+        $('h3', "#main-title").empty().text(newTitle);
+	$("#main-title").removeClass("editing");
+      }
     });
-
-    $("#save-title").live("click", function () {
-      var newTitle = $("#title-input").val();
-	  var data = "title=" + newTitle;
-	  data += isThread() ? "&thread_id=" + thread.id() : '';
-
-      $.ajax({
-        type: "POST",
-        url: "/title/edit",
-        data: data,
-        success: function(msg){
-          $('h3', "#main-title")
-            .empty()
-            .text(newTitle)
-		  $("#main-title")
-            .removeClass("editing");
-        }
-      });
-    });
-	$('#search-box').submit(function() {
-		window.location.href="find/" + $('#search-phrase').val();
-		return false;
-	});
-	$('#search-box-user').submit(function() {
-		window.location.href="users/0/" + $('#search-phrase-user').val();
-		return false;
-	});
-  })();
-
-(function () {
-  $('#toggle-html').bind('click', function(){
-    button = $(this);
-    $.get('/ajax/toggle_html/'+ session_id, function(data) {
-      window.location.reload(true);
-    });
-    return;
   });
 })();
 
-(function () {
-  $('#show_desktop').bind('click', function(){
-    button = $(this);
-    $.get('/ajax/show_desktop/'+ session_id, function(data) {
-      button.html(data);
-    });
-    return;
-  });
-})();
+$('#search-box').submit(function() {
+  window.location.href="find/" + $('#search-phrase').val();
+  return false;
+});
 
-(function () {
-  $('#show_mobile').bind('click', function(){
-    button = $(this);
-    $.get('/ajax/show_mobile/'+ session_id, function(data) {
-      button.html(data);
-    });
-    return;
-  });
-})();
+$('#search-box-user').submit(function() {
+  window.location.href="users/0/" + $('#search-phrase-user').val();
+  return false;
+});
 
+$('#toggle-html').bind('click', function(){
+  $.get('/ajax/toggle_html/'+ session_id, function(data) {
+    window.location.reload(true);
+  });
+});
+
+$('#show_desktop').bind('click', function(){
+  button = $(this);
+  $.get('/ajax/show_desktop/'+ session_id, function(data) {
+    button.html(data);
+  });
+});
+
+$('#show_mobile').bind('click', function(){
+  button = $(this);
+  $.get('/ajax/show_mobile/'+ session_id, function(data) {
+    button.html(data);
+  });
+});
 
 
 $('.hide-thread').bind('click', function(){
 
   var button = $(this);
   var toHide = $(this).hasClass('added');
-  var url = !toHide
-    ? '/ajax/hide_thread/'+ $(this).attr('rel') +'/'+ session_id
-    : '/ajax/unhide_thread/'+ $(this).attr('rel') +'/'+ session_id
+  var url = !toHide ? '/ajax/hide_thread/'+ $(this).attr('rel') +'/'+ session_id :
+    '/ajax/unhide_thread/'+ $(this).attr('rel') +'/'+ session_id;
 
   $.get(url, function(data) {
     if (data == 1) {
@@ -129,11 +113,10 @@ $('.favourite').bind('click', function(){
 });
 
 
-
-
 function isThread() {
   return (typeof(window.thread) == "undefined")?  false: true;
 }
+
 
 (function() {
 
