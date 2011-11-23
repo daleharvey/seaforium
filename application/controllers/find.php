2028cc9bccb0c58e19e43a00ccc7205cc691d6f4
Dale Harvey <dale@arandomurl.com>
2011-11-23 00:47:19 +0000

    minor refactoring

--- application/controllers/find.php
+++ application/controllers/find.php
@@ -29,52 +29,43 @@ class Find extends Controller {
   // echo "<pre style='margin-left: 300px'>";
    if($this->uri->segment(2)) {
 
+     $search_phrase = $this->uri->segment(2);
+     // get a thread count from the database
 
 
+     // how many threads per page
+     $display = $this->meta['threads_shown'] == false ? 50 : $this->meta['threads_shown'];
+     $filtering = $this->_ready_filters($filter, $ordering, $dir);
+     $thread_count = $this->thread_dal->find_thread_by_title_rows($search_phrase);
+     // init the pagination library
+     $this->pagination->initialize(array(
+       'base_url' => '/p/',
+       'total_rows' => $thread_count,
+       'uri_segment' => '2',
+       'per_page' => $display,
+       'suffix' => ''
+     ));
 
 
-	$search_phrase = $this->uri->segment(2);
-    // get a thread count from the database
+     // end of threads
+     $end = min(array($pagination + $display, $thread_count));
 
-
-    // how many threads per page
-    $display = $this->meta['threads_shown'] == false ? 50 : $this->meta['threads_shown'];
-	 $filtering = $this->_ready_filters($filter, $ordering, $dir);
-	$thread_count = $this->thread_dal->find_thread_by_title_rows($search_phrase);
-    // init the pagination library
-    $this->pagination->initialize(array(
-                                        'base_url' => '/p/',
-                                        'total_rows' => $thread_count,
-                                        'uri_segment' => '2',
-                                        'per_page' => $display,
-                                        'suffix' => ''
-                                        ));
-
-
-    // end of threads
-    $end = min(array($pagination + $display, $thread_count));
-
-    $this->load->view('threads', array(
-                                       'title' => $this->thread_dal->get_front_title(),
-                                       'thread_result' => $this->thread_dal->find_thread_by_title($this->meta['user_id'], $pagination, $display, $filtering['filter'], $filtering['order'], $search_phrase),
-                                       'pagination' => $this->pagination->create_links()
-                                       .'<span class="paging-text">' . ($pagination + 1) . ' - ' . $end . ' of ' . $thread_count . ' Threads</span>',
-                                       'tab_links' => strlen($filter) > 0 ? '/f/'.$filter.'/' : '/o/',
-                                       'tab_orders' => array(
-                                                             'started' => $ordering == 'started' && $dir == 'desc' ? 'asc' : 'desc',
-                                                             'latest' => $ordering == 'latest' && $dir == 'desc' ? 'asc' : 'desc',
-                                                             'posts' => $ordering == 'posts' && $dir == 'desc' ? 'asc' : 'desc'
-                                                             ),
-                                       'favorites' => explode(',', $this->thread_dal->get_favorites($this->meta['user_id'])),
-                                       'hidden_threads' => explode(',', $this->thread_dal->get_hidden($this->meta['user_id']))
-                                       ));
-
-
-	} else {
-		echo "derp";
-	}
-	//echo "</pre>";
-	$this->load->view('shared/footer');
+     $this->load->view('threads', array(
+       'title' => $this->thread_dal->get_front_title(),
+       'thread_result' => $this->thread_dal->find_thread_by_title($this->meta['user_id'], $pagination, $display, $filtering['filter'], $filtering['order'], $search_phrase),
+       'pagination' => $this->pagination->create_links()
+       .'<span class="paging-text">' . ($pagination + 1) . ' - ' . $end . ' of ' . $thread_count . ' Threads</span>',
+       'tab_links' => strlen($filter) > 0 ? '/f/'.$filter.'/' : '/o/',
+       'tab_orders' => array(
+         'started' => $ordering == 'started' && $dir == 'desc' ? 'asc' : 'desc',
+         'latest' => $ordering == 'latest' && $dir == 'desc' ? 'asc' : 'desc',
+         'posts' => $ordering == 'posts' && $dir == 'desc' ? 'asc' : 'desc'
+       ),
+       'favorites' => explode(',', $this->thread_dal->get_favorites($this->meta['user_id'])),
+       'hidden_threads' => explode(',', $this->thread_dal->get_hidden($this->meta['user_id']))
+      ));
+   }
+   $this->load->view('shared/footer');
   }
 
   function _ready_filters($filter, $ordering, $dir)
@@ -84,7 +75,7 @@ class Find extends Controller {
 
     return array(
       'filter' => $sql,
-      'order' => $sql_order,
+      'order' => "ORDER BY response_created DESC",
       'url_suffix' => (strlen($filter) > 0 ? '/'.$filter : '')
         . (strlen($ordering) > 0 ? '/'.$ordering : '')
         . (strlen($dir) > 0 ? '/'.$dir : '')
