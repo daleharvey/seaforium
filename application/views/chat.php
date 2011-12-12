29cf14adad84b7debd9013d76152059498b2cc06
Daniel <daniel.sibitzky@gmail.com>
2011-12-12 16:48:48 -0500

    dded in actual files for /chat

---
+++ application/views/chat.php
@@ -0,0 +1,12 @@
+<div id="main-title"><h3>Yay for TinyChat!</h3></div>
+
+<script type="text/javascript">
+var tinychat = {
+  room: "yh",
+  colorbk: "0xffffff",
+  join: "auto",
+  api: "list"
+  };
+</script>
+<script src="http://tinychat.com/js/embed.js"></script>
+<div id="tinychat"><a href="http://tinychat.com">video chat</a> provided by Tinychat</div>
