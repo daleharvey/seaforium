85435e243a18fe0f1d9bba2772bfe0322bc4fc26
Dale Harvey <dale@arandomurl.com>
2012-01-02 22:11:19 +0000

    Default to meaningless for unlogged in users

--- application/models/threadsmodel.php
+++ application/models/threadsmodel.php
@@ -3,12 +3,12 @@
 class Threadsmodel extends Model
 {
   var $args, $meta, $thread_count;
-  
+
   function __construct()
   {
     parent::__construct();
   }
-  
+
   /**
    * Get some threads from the database
    *
@@ -19,7 +19,7 @@ class Threadsmodel extends Model
   function get_threads($in = '')
   {
     $searching = !!$in;
-    
+
     $this->db->select('
       SQL_CALC_FOUND_ROWS
       threads.subject,
@@ -39,9 +39,9 @@ class Threadsmodel extends Model
         WHERE comments.thread_id = threads.thread_id
       ) AS response_count
       ', FALSE);
-    
+
     $this->db->from('threads');
-    
+
     $this->db->join('comments AS responses',
                     'responses.comment_id = threads.last_comment_id');
 
@@ -57,13 +57,17 @@ class Threadsmodel extends Model
     $this->db->join('acquaintances',
                     'acquaintances.acq_user_id = authors.id AND acquaintances.user_id = '. $this->meta['user_id']
                      , 'left');
-    
+
     if ($searching)
     {
       $this->db->where('threads.thread_id IN ('. $in .')');
     }
     else
     {
+      if (!$this->sauth->is_logged_in() && $this->args->filter === "") {
+        $this->args->filter = "meaningful";
+      }
+
       switch(strtolower($this->args->filter))
       {
         case 'discussions':
@@ -82,8 +86,8 @@ class Threadsmodel extends Model
           $this->db->where('threads.category !=', 4);
           break;
         case 'participated':
-          $this->db->where('threads.thread_id IN (SELECT DISTINCT comments.thread_id FROM comments, threads WHERE comments.user_id = '. $this->meta['user_id'] 
-            .' AND comments.thread_id = threads.thread_id AND threads.deleted = 0) AND NOT EXISTS (SELECT hidden_threads.hidden_id FROM hidden_threads WHERE hidden_threads.user_id = '. $this->meta['user_id'] 
+          $this->db->where('threads.thread_id IN (SELECT DISTINCT comments.thread_id FROM comments, threads WHERE comments.user_id = '. $this->meta['user_id']
+            .' AND comments.thread_id = threads.thread_id AND threads.deleted = 0) AND NOT EXISTS (SELECT hidden_threads.hidden_id FROM hidden_threads WHERE hidden_threads.user_id = '. $this->meta['user_id']
             .' AND hidden_threads.thread_id = threads.thread_id)');
           break;
         case 'favorites':
@@ -106,22 +110,22 @@ class Threadsmodel extends Model
           break;
       }
     }
-    
+
     // dont show hidden threads
     // unless thats what we're trying to do
     if ($this->args->filter != 'hidden')
       $this->db->where('NOT EXISTS (SELECT hidden_threads.hidden_id FROM hidden_threads WHERE hidden_threads.user_id = '. $this->meta['user_id']
         .' AND hidden_threads.thread_id = threads.thread_id)', null, false);
-    
+
       // dont want to show any deleted threads
     $this->db->where('threads.deleted', 0);
-    
+
     if (!$searching)
     {
       // get ordering direction
       if ($this->args->dir != 'desc' && $this->args->dir != 'asc')
         $this->args->dir = 'desc';
-    
+
       // order the threads
       if ($this->args->ordering == 'started') {
         $this->db->order_by('threads.created', $this->args->dir);
@@ -135,23 +139,23 @@ class Threadsmodel extends Model
       else {
         $this->db->order_by('response_created', 'DESC');
       }
-    
+
       // apply some limits to this thing
       $this->db->limit($this->args->pagination, $this->meta['threads_shown']);
     }
-    
+
     //echo $this->db->_compile_select();
-    
+
     // get the results
     $this->thread_results = $this->db->get();
-    
+
     // format out a url_suffix to use with pagination
-    $this->url_suffix = 
+    $this->url_suffix =
       (strlen($this->args->filter) > 0 ? '/'. $this->args->filter : '').
       (strlen($this->args->ordering) > 0 ? '/'. $this->args->ordering : '').
       (strlen($this->args->dir) > 0 ? '/'. $this->args->dir : '').
       (strlen($this->args->whostarted) > 0 ? '/'. $this->args->whostarted : '');
-    
+
     // get a count of all threads
     $this->thread_count = (int)$this->db->query('SELECT FOUND_ROWS() AS thread_count')->row()->thread_count;
   }
